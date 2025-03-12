b16bc80c3dc88f0d5240193fc7a38e36377ce25d
Moe-Thae-Nwe <129031734+Moe-Thae-Nwe@users.noreply.github.com>
2025-03-12 16:06:46 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom/client";
+import { marked } from "https://esm.sh/marked";
+import DOMPurify from "https://esm.sh/dompurify";
+
+let placeholderText = `# Header 
+
+## Sub header
+
+[a link](https://www.example.com)
+\`inline code\` 
+\`\`\`
+a code block
+\`\`\` 
+- a list item 
+> a blockquote 
+![an image](image.jpg)
+**bolded text**`
+
+marked.use({
+  gfm: true,
+  breaks: true
+})
+function App() {
+  const [text, setText] = React.useState(marked.parse(placeholderText))
+  function handleChange(event) {
+    let inputValue = marked.parse(event.target.value)
+    setText(inputValue)
+    console.log(text)
+  }
+  return(
+    <>
+      <textarea id = "editor" onChange = {handleChange}>{placeholderText}</textarea>
+      <div id = "preview" dangerouslySetInnerHTML = {{__html: text}}></div>
+   </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
