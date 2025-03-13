663614b0b1b66e9f720fa0539304312844abc7b9
Moe-Thae-Nwe <129031734+Moe-Thae-Nwe@users.noreply.github.com>
2025-03-13 18:00:50 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import React from "https://unpkg.com/react@17/umd/react.production.min.js"
-import ReactDOM from "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"
-import {marked} from "https://cdn.jsdelivr.net/npm/marked/marked.min.js"
-import DOMPurify from "https://cdn.jsdelivr.net/npm/dompurify@2.2.6/dist/purify.min.js"
+const { useState } = React;
+const { createRoot } = ReactDOM;
+const { marked } = window;
+const DOMPurify = window.DOMPurify;
 
 let placeholderText = `# Header 
 
@@ -15,27 +15,27 @@ a code block
 - a list item 
 > a blockquote 
 ![an image](image.jpg)
-**bolded text**`
+**bolded text**`;
 
 marked.use({
   gfm: true,
   breaks: true
-})
+});
 
 function App() {
-  const [text, setText] = React.useState(DOMPurify.sanitize(marked.parse(placeholderText)))
+  const [text, setText] = useState(DOMPurify.sanitize(marked.parse(placeholderText)));
   function handleChange(event) {
-    let inputValue = DOMPurify.sanitize(marked.parse(event.target.value))
-    setText(inputValue)
-    console.log(text)
+    let inputValue = DOMPurify.sanitize(marked.parse(event.target.value));
+    setText(inputValue);
+    console.log(text);
   }
-  
-  return(
+
+  return (
     <div>
-      <textarea id = "editor" onChange = {handleChange}>{placeholderText}</textarea>
-      <div id = "preview" dangerouslySetInnerHTML = {{__html: text}}></div>
-   </div>
-  )
+      <textarea id="editor" onChange={handleChange}>{placeholderText}</textarea>
+      <div id="preview" dangerouslySetInnerHTML={{ __html: text }}></div>
+    </div>
+  );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+createRoot(document.getElementById("root")).render(<App />);
